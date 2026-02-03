290dc156df8be746a2b9aaad6cbe960cd3213060
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2026-02-03 15:04:34 -0700

    Upgrade netCDF-Java and Apache SIS

--- common/src/main/java/uk/ac/rdg/resc/edal/util/GISUtils.java
+++ common/src/main/java/uk/ac/rdg/resc/edal/util/GISUtils.java
@@ -45,7 +45,7 @@ import javax.naming.Name;
 import javax.naming.spi.ObjectFactory;
 
 import org.apache.sis.geometry.DirectPosition2D;
-import org.apache.sis.metadata.sql.util.Initializer;
+import org.apache.sis.metadata.sql.internal.shared.Initializer;
 import org.apache.sis.metadata.iso.extent.DefaultGeographicBoundingBox;
 import org.apache.sis.referencing.CRS;
 import org.apache.sis.referencing.CommonCRS;
